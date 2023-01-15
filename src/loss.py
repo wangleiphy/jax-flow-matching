455b1a40a5a4dced973b6e33b6d4efe2a6231714
Lei Wang <wangleiphy@gmail.com>
2023-01-15 14:51:10 +0800

    circular linear interpolation

--- src/loss.py
+++ src/loss.py
@@ -7,11 +7,8 @@ def make_loss(vec_field_net, L):
     @partial(jax.vmap, in_axes=(None, 0, 0, 0), out_axes=0)
     def _matching(params, x0, x1, t):
         def circular_interpolation(x0, x1, t, L):
-            theta0 = x0*2*jnp.pi/L
-            theta1 = x1*2*jnp.pi/L
-            c = (1-t)*jnp.cos(theta0) + t*jnp.cos(theta1)
-            s = (1-t)*jnp.sin(theta0) + t*jnp.sin(theta1)
-            return jnp.arctan2(s, c)*L/(2*jnp.pi)
+            diff = x1-x0 
+            return jnp.where(diff<L/2, x0 + t*diff, x0+t*(diff-L))
 
         x = circular_interpolation(x0, x1, t, L)
         v = jax.vmap(jax.grad(circular_interpolation, argnums=2), (0, 0, None, None), 0)(x0, x1, t, L)
