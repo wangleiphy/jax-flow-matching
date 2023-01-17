453fce8bca8a8fb99e5b78f25c325356a34efeb2
Lei Wang <wangleiphy@gmail.com>
2023-01-17 13:42:59 +0800

    hollow net with L

--- src/hollow.py
+++ src/hollow.py
@@ -1,13 +1,10 @@
 '''
-hallow net with cheap divergence operator https://arxiv.org/abs/1912.03579
+hollow net with cheap divergence operator https://arxiv.org/abs/1912.03579
 in addition, we make this permutation equvariant 
 '''
-
 import jax
 import jax.numpy as jnp
 import haiku as hk
-
-from maskedlinear import MaskedLinear
 
 def make_conditioner(num_heads, key_size, model_size):
     def conditioner(x):
@@ -22,36 +19,37 @@ def make_conditioner(num_heads, key_size, model_size):
         return mha(x, x, x, mask=mask)
     return conditioner
 
-def make_transformer(hidden_sizes):
+def make_transformer(hidden_sizes, dim):
     def transformer(x, h):
-        dim = x.shape[0]
-        mask = jnp.eye(dim, dtype=bool)
-        x_repeat = jnp.repeat(x[None, :], dim, axis=0) # (d, d)
+        mask = jnp.kron(jnp.ones(2), jnp.eye(dim)) # 2 because we do cos/sin
+        x_repeat = jnp.repeat(x[None, :], dim, axis=0) # (d, 2d)
         x_repeat = jnp.where(mask, x_repeat, jax.lax.stop_gradient(x_repeat)) 
-        y = jnp.concatenate([x_repeat, h.reshape(dim, -1)], axis=1) # (d, d+h)
+        y = jnp.concatenate([x_repeat, h.reshape(dim, -1)], axis=1) # (d, 2d+h)
         return hk.nets.MLP(hidden_sizes+[1], activation=jax.nn.softplus)(y)
 
     return transformer
 
-def make_hallow_net(hidden_sizes):
+def make_hollow_net(hidden_sizes, L):
 
     @hk.without_apply_rng
     @hk.transform
-    def hallow_net(x): 
+    def hollow_net(x): 
         n, dim = x.shape
         conditioner = make_conditioner(8, 16, dim*16)
-        transformer = make_transformer(hidden_sizes)
+        transformer = make_transformer(hidden_sizes, dim)
+        x = jnp.concatenate([jnp.cos(2*jnp.pi*x/L), 
+                             jnp.sin(2*jnp.pi*x/L), 
+                             ], axis=1) # (n, 2d)
         h = conditioner(x) # (n, d*h) 
         h = jax.lax.stop_gradient(h)
-        return hk.vmap(transformer, split_rng=False)(x,h) # (n, dim)
+        return hk.vmap(transformer, split_rng=False)(x,h) # (n, d)
 
-    return hallow_net
+    return hollow_net
 
-def make_divergence_fn(network, n, dim):
+def make_divergence_fn(network):
 
     def divergence_fn(params, x):
-        f = lambda x: network.apply(params, x.reshape(n, dim)).reshape(-1)
-        x = x.reshape(-1)
+        f = lambda x: network.apply(params, x)
         _, jvp = jax.jvp(f, (x,), (jnp.ones_like(x),))
         return jnp.sum(jvp)
 
--- tests/test_hollow.py
+++ tests/test_hollow.py
@@ -1,19 +1,20 @@
 from config import * 
 
-from hallow import make_hallow_net, make_divergence_fn
+from hollow import make_hollow_net, make_divergence_fn
 from utils import divergence_fwd
 
 def test_symmetry():
     n = 8
     dim = 3
+    L = 1.234
     hidden_sizes = [16, 16]
 
     key = jax.random.PRNGKey(42)
 
-    network = make_hallow_net(hidden_sizes)
+    network = make_hollow_net(hidden_sizes, L)
     params = network.init(key, jnp.zeros((n, dim)))
 
-    x = jax.random.normal(key, (n, dim))
+    x = jax.random.uniform(key, (n, dim), minval=0, maxval=L)
     v = network.apply(params, x)
 
     P = np.random.permutation(n)
@@ -21,25 +22,30 @@ def test_symmetry():
 
     assert jnp.allclose(Pv, v[P, :])
 
+    Tv = network.apply(params, x+L)
+    assert jnp.allclose(Tv, v)
+
 def test_div():
-    n = 4
-    dim = 3
+    n = 3
+    dim = 2
+    L = 1.234
     hidden_sizes = [16, 16]
 
     key = jax.random.PRNGKey(42)
 
-    network = make_hallow_net(hidden_sizes)
+    network = make_hollow_net(hidden_sizes, L)
     params = network.init(key, jnp.zeros((n, dim)))
 
-    x = jax.random.normal(key, (n, dim))
+    x = jax.random.uniform(key, (n, dim), minval=0, maxval=L)
     
     f = lambda x: network.apply(params, x.reshape(n, dim)).reshape(-1)
     div = divergence_fwd(f)(x.reshape(-1))
     
-    div_fn = make_divergence_fn(network, n, dim)
+    div_fn = make_divergence_fn(network)
     div2 = div_fn(params, x)
     
     print (div, div2)
+    print (jax.jacfwd(f)(x.reshape(-1)))
     assert (jnp.allclose(div, div2))
 
 test_div()
