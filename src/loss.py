e31f219de49fc092fe8edad37150c2e36061c688
Lei Wang <wangleiphy@gmail.com>
2023-02-02 09:20:38 +0800

    fix wrapped cauchy interpolation

--- src/loss.py
+++ src/loss.py
@@ -7,7 +7,7 @@ def make_loss(vec_field_net, L):
     @partial(jax.vmap, in_axes=(None, 0, 0, 0), out_axes=0)
     def _matching(params, x0, x1, t):
         def circular_interpolation(x0, x1, t, L):
-            if True:
+            if False:
                 diff = x1-x0 
                 return jnp.where(jnp.abs(diff)<L/2, 
                                  x0 + t*diff, 
@@ -15,13 +15,15 @@ def make_loss(vec_field_net, L):
                                 )
             else: 
                 # wrapped cauchy p. 473. http://www.nrbook.com/devroye/Devroye_files/chapter_nine.pdf
-                rho0, rho1 = 0.1, 0.9
+                eps = 1e-3
+                rho0, rho1 = eps, 1-eps
                 rho = rho0 * (1-t) + rho1 * t
                 mu = x1 * t 
                 s = (1.0 + rho**2) / (2.0 * rho)
                 u = 2*x0 - 1 # U(-1, 1)
                 z = jnp.cos(jnp.pi*u)
                 w = (1.0 + s * z) / (s + z)
+                w = jnp.clip(w, a_min=-1+eps, a_max=1-eps) # clip to avoid numerical error 
                 x = jnp.sign(u) * jnp.arccos(w) # (-pi, pi)
                 return x*L/(2*jnp.pi) + mu
 
